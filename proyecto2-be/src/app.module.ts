ea766640e120cc219eb67d5ea460576f1d803787
Ayoubb4 <ayoubmisbahfat.al19@emiliojimeno.edu.es>
2025-04-07 08:58:18 +0200

    Eliminar archivos no deseados del repositorio

--- proyecto2-be/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SpotifyController } from './spotify/spotify.controller';
-import { SpotifyService } from './spotify/spotify.service';
-
-@Module({
-  controllers: [SpotifyController],
-  providers: [SpotifyService],
-})
-export class AppModule {}
