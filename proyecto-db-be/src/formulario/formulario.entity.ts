919bdb796c242d5285872c43f834f30552606fdf
Ayoubb4 <ayoubmisbahfat.al19@emiliojimeno.edu.es>
2025-04-10 08:54:56 +0200

    :art: Improved Formulario entity with backend validation
    - Added detailed validation rules using class-validator for all fields:
      - nombre: Validates string type, length (2-50), and ensures proper input.
      - apellidos: Checks string type, length (2-50), and restricts input to letters and spaces.
      - correo: Validates email format using @IsEmail decorator.

--- proyecto-db-be/src/formulario/formulario.entity.ts
+++ proyecto-db-be/src/formulario/formulario.entity.ts
@@ -1,16 +1,24 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsEmail, IsString, Matches, Length } from 'class-validator';
 
 @Entity()
 export class Formulario {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @IsString()
+  @Length(2, 50, { message: 'El nombre debe tener entre 2 y 50 caracteres.' })
   @Column()
   nombre: string;
 
+  @IsString()
+  @Matches(/^[a-zA-Z\s]+$/, {
+    message: 'Apellidos solo puede contener letras y espacios.',
+  })
   @Column()
   apellidos: string;
 
+  @IsEmail({}, { message: 'El correo no tiene un formato válido.' })
   @Column()
   correo: string;
 
